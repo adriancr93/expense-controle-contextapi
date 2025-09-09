32b6fea921ab8804476910f70661c92563a09584
Adrian Obregon <adriancr93@hotmail.com>
2025-09-09 17:50:48 -0600

    Set up the project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
+import BudgetForm from "./components/BudgetForm"
 
 function App() {
 
 
   return (
     <>
-      <h1>Expense Controle</h1>
+      <header className="bg-blue-600 py-8 max-h-72">
+        <h1 className="uppercase text-center font-black text-4xl text-white">
+           Expense Planner
+        </h1>
+      </header>
+      
+        <div className="max-w-3xl mx-auto bg-white shadow-lg roundend-lg mt-10 p-10">
+           <BudgetForm />
+        </div>
+      
     </>
   )
 }
